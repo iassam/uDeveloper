63f44621df8361ad0b736a4e97a87134748e4e49
Iassam <iassam@users.noreply.github.com>
2019-02-19 01:00:05 -0500

    Autoconfig mysql sercure password

--- ubuntu.sh
+++ ubuntu.sh
@@ -122,6 +122,32 @@ LAMP(){
     echo -e "\n--- Install bower ---\n"
     npm install -g gulp bower
     service apache2 restart
+    
+    apt-get install expect -y
+
+    MYSQL_ROOT_PASSWORD=$MYSQL_PASSWD
+
+        SECURE_MYSQL=$(expect -c "
+        set timeout 10
+        spawn mysql_secure_installation
+        expect \"Enter current password for root (enter for none):\"
+        send \"$MYSQL\r\"
+        expect \"Change the root password?\"
+        send \"n\r\"
+        expect \"Remove anonymous users?\"
+        send \"y\r\"
+        expect \"Disallow root login remotely?\"
+        send \"y\r\"
+        expect \"Remove test database and access to it?\"
+        send \"y\r\"
+        expect \"Reload privilege tables now?\"
+        send \"y\r\"
+        expect eof
+        ")
+
+    echo "$SECURE_MYSQL"
+
+    apt-get -y purge expect
 
     echo "LAMP installed."
 }
